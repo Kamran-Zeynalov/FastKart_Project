c7328a4363488104f63e0cb40bea34fcab7062d0
Kamran Z <kamraneze@code.edu.az>
2023-10-09 17:48:10 +0400

    Added Header and Footer

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+
+function Home(){
+    return(
+        <>
+        <Header/>
+        <Footer />
+        
+            </>
+    )
+}
+
+
+export default Home;
